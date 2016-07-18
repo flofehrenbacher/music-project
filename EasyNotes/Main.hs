4d319fc10bc3216258f5945c299c123001c5e86d
Florian Fehrenbacher <Florian Fehrenbacher>
2016-07-18 23:47:20 +0200

    fixed TabCharacters and Modus

--- EasyNotes/Main.hs
+++ EasyNotes/Main.hs
@@ -16,12 +16,13 @@ import SongCollection
 import Data.IORef
 import Euterpea.IO.MIDI.MidiIO
 import Graphics.UI.GLUT
+import Text.Read
 
 main :: IO ()
 main = do
     (progName, args) <- getArgsAndInitialize
     arguments <- setModusAndSong args
-    startWithArguments arguments
+    startWithArguments arguments progName
 
 idle ::  MyTime        -> (InputDeviceID,OutputDeviceID) -> IORef DisplayInfo -> IdleCallback
 idle     startTimeRef     (inputID, outputID)              displayInfoRef = do
@@ -45,24 +46,24 @@ reshape (Size width height) = do
     viewport $= (Position ((width - 700) `div` 2)  ((height - 500) `div` 2), (Size 700 500))
     postRedisplay Nothing
 
-startWithArguments :: (Maybe Modus, Maybe Song) -> IO()
-startWithArguments    (Just myModus, Just song)         = do
-	initialDisplayMode $= [DoubleBuffered]
-	initialWindowSize $= Size 700 500
-	createWindow progName
-	(displayInfoRef, startTimeRef) <- setUpIORefs song
-	(inputID, outputID) <- initDevices
-	mouseCallback 	$= Just (mouse displayInfoRef outputID)
-	idleCallback    $= Just    (idle startTimeRef (inputID, outputID) displayInfoRef)
-	displayCallback $= display displayInfoRef myModus
-	reshapeCallback $= Just reshape
-	mainLoop
-startWithArguments  _                                    = do
-	putStrLn "The first argument must be: easy, medium or hard"
-	putStrLn "The second one must be the song you want to learn" 
-	putStrLn "AlleMeineEntchen, HaenschenKlein, MadWorld, AllNotes"
+-- startWithArguments :: (Maybe Modus, Maybe Song) -> IO()
+startWithArguments    (Just myModus, Just song)  progName       = do
+    initialDisplayMode $= [DoubleBuffered]
+    initialWindowSize $= Size 700 500
+    createWindow progName
+    (displayInfoRef, startTimeRef) <- setUpIORefs song
+    (inputID, outputID) <- initDevices
+    mouseCallback   $= Just (mouse displayInfoRef outputID)
+    idleCallback    $= Just    (idle startTimeRef (inputID, outputID) displayInfoRef)
+    displayCallback $= display displayInfoRef myModus
+    reshapeCallback $= Just reshape
+    mainLoop
+startWithArguments  _                 _                   = do
+    putStrLn "The first argument must be: easy, medium or hard"
+    putStrLn "The second one must be the song you want to learn" 
+    putStrLn "AlleMeineEntchen, HaenschenKlein, MadWorld, AllNotes"
 
 setModusAndSong :: [String]    -> IO (Maybe Modus, Maybe Song)
 setModusAndSong    (myModus : song : []) = do
-    return $ (lookup myModus modi, lookup song songCollection)
+    return $ (readMaybe myModus, lookup song songCollection)
 setModusAndSong    _             = return (Nothing,Nothing)
