78fe228611bec0874ad7fa9b2ce942c04f71f5da
Florian Fehrenbacher <Florian Fehrenbacher>
2016-08-01 13:41:53 +0200

    added docu to Modus

--- EasyNotes/Modus.hs
+++ EasyNotes/Modus.hs
@@ -3,5 +3,7 @@ module Modus where
 data Modus = Easy | Medium | Hard
     deriving (Eq,Read,Show,Enum)
 
+-- | puts all modi into a list of strings
+-- the first data type of Modus MUST be Easy
 getAllModi :: [String]
 getAllModi = map show $ enumFrom Easy
