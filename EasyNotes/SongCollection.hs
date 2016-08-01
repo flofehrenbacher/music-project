4e3937644df5497bc433a2db10d6b65c283e543d
Florian Fehrenbacher <Florian Fehrenbacher>
2016-08-01 13:42:04 +0200

    added docu to SongCollection

--- EasyNotes/SongCollection.hs
+++ EasyNotes/SongCollection.hs
@@ -10,14 +10,17 @@ import Euterpea
 
 type Song = [Music Pitch]
 
+-- | the current collection of songs as tuples of songnames and their corresponding list of Music Pitches
 songCollection :: [(String,Song)]
 songCollection = [("AlleMeineEntchen",entchenList),
                   ("AllNotes",allNotes),
                   ("HaenschenKlein", haenschenList),
                   ("MadWorld",madList)]
 
+-- | list of all names of the current song collection
 allSongs :: [String]
 allSongs  =  map fst songCollection
 
+-- | song which contains all notes from C4 to B4
 allNotes :: Song
 allNotes  = map (pitchToMusic qn) $ map pitch [60..71]
