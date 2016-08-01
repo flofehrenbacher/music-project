350f0d9752edbfba389588988ba86eaf83a7134a
Florian Fehrenbacher <Florian Fehrenbacher>
2016-08-01 13:42:20 +0200

    added docu to Time

--- EasyNotes/Time.hs
+++ EasyNotes/Time.hs
@@ -3,6 +3,7 @@ module Time where
 import Data.Time.Clock
 import Data.IORef
 
+-- | computes the difference between the current time and the specified time
 computePassedTime :: IORef UTCTime -> IO NominalDiffTime
 computePassedTime    startTimeRef  = do
     time <- getCurrentTime
