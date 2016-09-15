961333bc1797a534f5f31bcdd6dbee9d735c86d9
Florian Fehrenbacher <Florian Fehrenbacher>
2016-09-15 16:33:19 +0100

    create new module for pitch classes

---
+++ EasyNotes/PitchClass.hs
@@ -0,0 +1,27 @@
+module PitchClass where
+
+import Euterpea
+import Graphics.UI.GLUT
+
+pitchInformation :: PitchClass -> (String, GLfloat)
+pitchInformation    pitchClass =  (show pitchClass, heightOf pitchClass)
+
+
+heightOf :: PitchClass -> GLfloat
+heightOf pitchClass = initialHeight + (0.025 * (heightMainNotes (takeMainPitchClass pitchClass)))
+    where initialHeight = -0.05
+
+heightMainNotes :: PitchClass -> GLfloat
+heightMainNotes    C          =  0
+heightMainNotes    D          =  1
+heightMainNotes    E          =  2
+heightMainNotes    F          =  3
+heightMainNotes    G          =  4
+heightMainNotes    A          =  5
+heightMainNotes    B          =  6
+
+takeMainPitchClass :: PitchClass -> PitchClass
+takeMainPitchClass    pitchClass = read $ charToString $ Prelude.head $ show pitchClass
+
+charToString :: Char -> String
+charToString = (:[])
