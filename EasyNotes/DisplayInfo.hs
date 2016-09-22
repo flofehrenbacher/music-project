b0cf05a6b7be8d1564e2537ca5755c6dcb2ba199
Florian Fehrenbacher <Florian Fehrenbacher>
2016-09-22 20:22:30 +0100

    restructuring and bug fixing (same notes weren't repeated)

--- EasyNotes/DisplayInfo.hs
+++ EasyNotes/DisplayInfo.hs
@@ -1,21 +1,25 @@
 module DisplayInfo where
 
-import Data.IORef
-import Data.Time.Clock
-import Euterpea
-import Graphics.UI.GLUT
-
 import SongCollection
 import Songs.AuxilaryFunctions
 import Time
 import View.Text
 import View.Key
 
+import Data.IORef
+import Data.Time.Clock
+import Euterpea
+import Graphics.UI.GLUT
+
+initialNotePlace :: GLfloat
+initialNotePlace = 1.4
+
 -- | contains all the information which is needed to display the current state
 data DisplayInfo = DisplayInfo {isRightNotePlayed :: Bool,
-                                isKeyPressed :: Bool,
+                                isScreenKeyPressed :: Bool,
                                 isMidiKeyPressed :: Bool,
                                 songInfo :: Maybe (PitchClass, [Music Pitch]),
+                                currentNote :: Maybe PitchClass,
                                 lastNote :: Maybe PitchClass,
                                 notePlace :: GLfloat}
                                   deriving (Eq,Show)
@@ -24,11 +28,12 @@ data DisplayInfo = DisplayInfo {isRightNotePlayed :: Bool,
 initializeDisplayInfo :: Song -> IO (IORef DisplayInfo, IORef UTCTime)
 initializeDisplayInfo    song = do
     displayInfoRef <- newIORef $ DisplayInfo {isRightNotePlayed = False,
-                                              isKeyPressed = False,
+                                              isScreenKeyPressed = False,
                                               isMidiKeyPressed = False,
                                               songInfo = updateSongInfo song,
                                               lastNote = Nothing,
-                                              notePlace = 1.4}
+                                              currentNote = Nothing,
+                                              notePlace = initialNotePlace}
     curTime        <- getCurrentTime
     startTimeRef   <- newIORef curTime
     return (displayInfoRef,startTimeRef)
@@ -36,7 +41,7 @@ initializeDisplayInfo    song = do
 -- | updates the song information, speaking returns a tuple containing the next pitch class which has to be played
 -- and a list of the remaining notes of the song
 -- if the song has finished returns Nothing
-updateSongInfo :: [Music Pitch]                              -> Maybe (PitchClass, [Music Pitch])
+updateSongInfo :: Song                                      -> Maybe (PitchClass, [Music Pitch])
 updateSongInfo    []                                         =  Nothing
 updateSongInfo    ((Prim (Note _ ((pitchClass,_)))) : notes) =  (Just (pitchClass, notes))
 updateSongInfo    ((Prim (Rest _)) : notes)                  =  updateSongInfo notes
@@ -50,20 +55,28 @@ updateDisplayInfo    displayInfo     startTimeRef      currentPitchClassPlayed  
             let remainingNotes = (fmap snd (songInfo displayInfo))
             displayInfo' <- updateLastNote currentPitchClassPlayed displayInfo
             -- RIGHT NOTE WAS PLAYED
-            if (isKeyPressed displayInfo' || isMidiKeyPressed displayInfo') && isAbsPitchTheSame nextNote (lastNote displayInfo') then do
+            if (isScreenKeyPressed displayInfo' || isMidiKeyPressed displayInfo') && isAbsPitchTheSame nextNote (lastNote displayInfo') then do
                 let newDisplayInfo = displayInfo' {isRightNotePlayed = True,
-                                                  songInfo = updateSongInfo =<< remainingNotes}
+                                                  songInfo = updateSongInfo =<< remainingNotes,
+                                                  lastNote = Nothing}
                 resetTime startTimeRef
                 return newDisplayInfo
             -- WRONG NOTE WAS PLAYED
             else do
-                case (isKeyPressed displayInfo' || isMidiKeyPressed displayInfo') of
+                case (isScreenKeyPressed displayInfo' || isMidiKeyPressed displayInfo') of
                     False -> return displayInfo' {isRightNotePlayed = False}
                     True -> do
                         return $ displayInfo'
 
--- | updates the last note that was played in the display information
+-- | updates in the display information the last note that was played
 updateLastNote ::  Maybe PitchClass -> DisplayInfo     -> IO DisplayInfo
 updateLastNote     (Just pitchClass)   displayInfo = do
-            return (displayInfo {lastNote = Just pitchClass})
+            return (displayInfo {lastNote = Just pitchClass, currentNote = Just pitchClass})
 updateLastNote     _                   displayInfo = return displayInfo
+
+-- | updates in the display information if a key is currently pressed on the screen
+keyPressed :: IORef DisplayInfo -> Bool    -> IO ()
+keyPressed    displayInfoRef       press   = do
+    displayInfo <- readIORef displayInfoRef
+    let newDisplayInfo = displayInfo {isScreenKeyPressed = press}
+    displayInfoRef $= newDisplayInfo
